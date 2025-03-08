2a341f12b587ce53da0619b6e6ab2f2d7de768d0
JayPatel5999 <Jpatel9102@conestogac.on.ca>
2025-03-08 02:30:40 -0500

    Browser Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Hospital from "./Components/Hospital"
 
 function App() {
 	return (
-		<div className="App">
-			<Hospital />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/hospital_info"
+					element={
+						<div className="App">
+							<Hospital />
+						</div>
+					}
+					title="Hospital Information"></Route>
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
